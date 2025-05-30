8d3fe2e5cfff707e791433ed685d5315203371e7
Komal Tayade <94972236+komaltayade@users.noreply.github.com>
2025-05-30 16:38:05 +0530

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,28 @@
+#4.Evaluate model using MAE, MSE, R²
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+df = pd.read_csv(r"C:\Users\Kamini Shewale\OneDrive\Desktop\AI-ML(internship)\task3\Housing.csv")
+
+print(df.columns)
+
+X = df[['area']]      
+y = df['price']    
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"R² Score: {r2}")
+
